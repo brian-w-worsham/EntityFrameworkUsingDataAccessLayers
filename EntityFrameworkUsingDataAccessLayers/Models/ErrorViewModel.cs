4bcca0ce4cb0f8e3a96cbb2b0e714062f75c7112
Brian Worsham <brian.w.worsham@hotmail.com>
2023-08-07 20:46:09 -0500

    Add project files.

---
+++ EntityFrameworkUsingDataAccessLayers/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EntityFrameworkUsingDataAccessLayers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
